e45bff0cfb6ed3407c2d7777e42c3e8dde8c7b18
Fraankz <frankq002@gmail.com>
2025-04-27 17:15:28 +0200

    Integrado MenuView y MenuController en ArcadeLauncher, así se sabe que juego ha seleccionado el usuario

--- src/main/java/arcade/app/ArcadeLauncher.java
+++ src/main/java/arcade/app/ArcadeLauncher.java
@@ -1,10 +1,12 @@
 package arcade.app;
 
+import arcade.controller.MenuController;
 import arcade.view.MenuView;
 
 public class ArcadeLauncher {
     public void start() {
         MenuView menuView = new MenuView();
-        menuView.showMenu();
+        menuView.showMenu(); // Primero mostramos y creamos los botones
+        MenuController menuController = new MenuController(menuView); // Luego creamos el controlador
     }
 }
--- src/main/java/arcade/controller/MenuController.java
+++ src/main/java/arcade/controller/MenuController.java
@@ -1,4 +1,53 @@
 package arcade.controller;
 
+import arcade.view.MenuView;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MenuController {
+    private MenuView menuView;
+
+    public MenuController(MenuView menuView) {
+        this.menuView = menuView;
+        initController();
+    }
+
+    private void initController() {
+        // Asignar acciones a cada botón
+        menuView.getNQueensButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onNQueensSelected();
+            }
+        });
+
+        menuView.getKnightTourButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onKnightTourSelected();
+            }
+        });
+
+        menuView.getHanoiButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onHanoiSelected();
+            }
+        });
+    }
+
+    private void onNQueensSelected() {
+        JOptionPane.showMessageDialog(null, "Has seleccionado el Problema de N Reinas");
+        // Aquí luego se llamará a GameFactory para lanzar el juego
+    }
+
+    private void onKnightTourSelected() {
+        JOptionPane.showMessageDialog(null, "Has seleccionado el Recorrido del Caballo");
+    }
+
+    private void onHanoiSelected() {
+        JOptionPane.showMessageDialog(null, "Has seleccionado las Torres de Hanói");
+    }
 }
